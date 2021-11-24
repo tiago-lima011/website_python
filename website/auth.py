37978568897e082ec82fe0778a05ba7c953cbd76
Tiago Lima <tiagolimalima57@gmail.com>
2021-11-24 15:23:15 +0000

    sign_up

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,35 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    data = request.form
+    print(data)
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
-    return "<p>You now logged out<p/>"
+    return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign Up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            pass
+        elif len(firstName) <2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            # add user to database
+            pass
+    return render_template("sign_up.html")
 
