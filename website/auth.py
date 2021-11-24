78781e544e5589316a23c1dca1691a8ec90af54a
Tiago Lima <tiagolimalima57@gmail.com>
2021-11-24 17:40:48 +0000

    auth_create user

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 auth = Blueprint('auth', __name__)
 
@@ -23,14 +26,17 @@ def sign_up():
         if len(email) < 4:
             flash('Email must be greater than 4 charaters.', category='error')
         elif len(firstName) <2:
-            flash('firstName must be greater than 1 charaters.', category='error')
+            flash('firstName must be greater than 1 charater.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 charaters.', category='error')
         else:
-            # add user to database
+            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
-            
+            return redirect(url_for('views.home'))
+
     return render_template("sign_up.html")
 
